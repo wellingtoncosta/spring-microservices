3426022715bc75c9029dcc28ee80fb000f49f3f7
Wellington Costa <wellington.costa128@gmail.com>
2018-05-14 15:37:14 -0300

    Initial microservices configuration

---
+++ book-ms/src/main/kotlin/br/com/wellingtoncosta/bookms/Application.kt
@@ -0,0 +1,15 @@
+package br.com.wellingtoncosta.bookms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+/**
+ * @author Wellington Costa on 14/05/18.
+ */
+@SpringBootApplication
+class Application : SpringBootServletInitializer()
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
---
+++ reservation-ms/src/main/kotlin/br/com/wellingtoncosta/reservationms/Application.kt
@@ -0,0 +1,25 @@
+package br.com.wellingtoncosta.clientms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+/**
+ * @author Wellington Costa on 14/05/18.
+ */
+@SpringBootApplication
+class Application : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(Application::class.java)
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<Application>(*args)
+        }
+    }
+
+}
