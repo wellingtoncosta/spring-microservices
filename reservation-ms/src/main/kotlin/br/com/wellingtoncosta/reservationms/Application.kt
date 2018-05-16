f84ac405aa284b409260a96fb8d80bf2ffae2b77
Wellington Costa <wellington.costa128@gmail.com>
2018-05-16 17:49:22 -0300

    Configuring dependencies and the servers ports.

--- reservation-ms/src/main/kotlin/br/com/wellingtoncosta/reservationms/Application.kt
+++ reservation-ms/src/main/kotlin/br/com/wellingtoncosta/reservationms/Application.kt
@@ -1,7 +1,6 @@
-package br.com.wellingtoncosta.clientms
+package br.com.wellingtoncosta.reservationms
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
@@ -9,17 +8,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @author Wellington Costa on 14/05/18.
  */
 @SpringBootApplication
-class Application : SpringBootServletInitializer() {
+class Application : SpringBootServletInitializer()
 
-    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
-        return application.sources(Application::class.java)
-    }
-
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            runApplication<Application>(*args)
-        }
-    }
-
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
 }
