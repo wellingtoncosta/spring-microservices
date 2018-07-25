e3e44dc41207e98deb546ec0bd2fcdf62cae1918
Wellington Costa <wellington.costa128@gmail.com>
2018-07-25 14:40:11 -0300

    Configuring Eureka Service Registry.

--- book-service/src/main/kotlin/br/com/wellingtoncosta/book/BookApplication.kt
+++ book-service/src/main/kotlin/br/com/wellingtoncosta/book/BookApplication.kt
@@ -2,10 +2,12 @@ package br.com.wellingtoncosta.book
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 /**
  * @author Wellington Costa on 14/05/18.
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 class BookApplication
 
--- service-registry/src/main/kotlin/br/com/wellingtoncosta/discovery/ServiceDiscoveryApplication.kt
+++ service-registry/src/main/kotlin/br/com/wellingtoncosta/discovery/ServiceDiscoveryApplication.kt
@@ -2,15 +2,15 @@ package br.com.wellingtoncosta.reservation
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 /**
  * @author Wellington Costa on 14/05/18.
  */
-@EnableDiscoveryClient
+@EnableEurekaServer
 @SpringBootApplication
-class ReservationApplication
+class ServiceDiscoveryApplication
 
 fun main(args: Array<String>) {
-    runApplication<ReservationApplication>(*args)
+    runApplication<ServiceDiscoveryApplication>(*args)
 }
