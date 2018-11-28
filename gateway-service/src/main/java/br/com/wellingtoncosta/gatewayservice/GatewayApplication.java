7321e873aa61c9c1baee710fd59dac2066ed4841
Wellington Costa <wellington.costa128@gmail.com>
2018-11-28 16:42:50 -0200

    Configuring API Gateway witho Spring Cloud Gateway.

---
+++ gateway-service/src/main/java/br/com/wellingtoncosta/gatewayservice/GatewayApplication.java
@@ -0,0 +1,18 @@
+package br.com.wellingtoncosta.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Wellington Costa on 28/11/2018.
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
