81a84bf941658d1fad91b01e3c43c698186ede1e
Wellington Costa <wellington.costa128@gmail.com>
2018-11-30 17:20:40 -0200

    Configuring database persistence using H2.

---
+++ book-service/src/main/java/br/com/wellingtoncosta/bookservice/configuration/DatabaseConfiguration.java
@@ -0,0 +1,35 @@
+package br.com.wellingtoncosta.bookservice.configuration;
+
+import br.com.wellingtoncosta.bookservice.BookApplication;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Wellington Costa on 30/11/2018.
+ */
+@Configuration
+@EnableJpaRepositories
+@EnableTransactionManagement
+@ComponentScan(basePackageClasses = { BookApplication.class })
+public class DatabaseConfiguration {
+
+    @Bean public DataSource dataSource(DataSourceProperties dataSourceProperties) {
+        HikariConfig config = new HikariConfig();
+
+        config.setDriverClassName(dataSourceProperties.getDriverClassName());
+        config.setJdbcUrl(dataSourceProperties.getUrl());
+        config.setUsername(dataSourceProperties.getUsername());
+        config.setPassword(dataSourceProperties.getPassword());
+
+        return new HikariDataSource(config);
+    }
+
+}
---
+++ reservation-service/src/main/java/br/com/wellingtoncosta/reservationservice/configuration/DatabaseConfiguration.java
@@ -0,0 +1,32 @@
+package br.com.wellingtoncosta.reservationservice.configuration;
+
+import br.com.wellingtoncosta.reservationservice.ReservationApplication;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories
+@EnableTransactionManagement
+@ComponentScan(basePackageClasses = { ReservationApplication.class })
+public class DatabaseConfiguration {
+
+    @Bean public DataSource dataSource(DataSourceProperties dataSourceProperties) {
+        HikariConfig config = new HikariConfig();
+
+        config.setDriverClassName(dataSourceProperties.getDriverClassName());
+        config.setJdbcUrl(dataSourceProperties.getUrl());
+        config.setUsername(dataSourceProperties.getUsername());
+        config.setPassword(dataSourceProperties.getPassword());
+
+        return new HikariDataSource(config);
+    }
+
+}
